dbe6e4b5e593285182c893495e9c2962f878d647
bruce grover lee <brucegrover.lee@gmail.com>
2019-08-09 16:14:49 -0500

    fix name

--- jenkins/scripts/development.deploy.sh
+++ jenkins/scripts/development.deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-gcloud auth activate-service-account --key-file $GCLOUD_SERVICE_ACCOUNT
+gcloud auth activate-service-account --key-file $SHERPON_DEVELOPMENT_SERVICE_ACCOUNT_KEY
 gcloud functions deploy functionJenkins \
   --runtime nodejs8 \
   --trigger-http \
